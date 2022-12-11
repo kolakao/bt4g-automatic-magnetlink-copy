9b11dabd4cf1f4cac40777fcbc52d9586ea20c23
kolakao <pozdro600lol900@o2.pl>
2022-12-11 14:03:09 +0100

    auto magnetlink Hello 0.0.1

--- bt4gautomagnetcopy.meta.js
+++ bt4gautomagnetcopy.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name    BT4G Automatic magnetlink copier
-// @version    0.0.1
+// @name         BT4G Automatic magnetlink copier
+// @version      0.0.1
 // @namespace    https://github.com/kolakao
 // ==/UserScript==
--- bt4gautomagnetcopy.user.js
+++ bt4gautomagnetcopy.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name    BT4G Automatic magnetlink copier
-// @version    0.0.1
+// @name         BT4G Automatic magnetlink copier
+// @version      0.0.1
 // @namespace    https://github.com/kolakao
 
 // @description  try to take over the world!
@@ -8,8 +8,8 @@
 // @match        http://bt4g.org/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
-// @updateURL    https://github.com/kolakao/bt4g-clicker/blob/master/bt4gautomagnetcopy.meta.js
-// @downloadURL  https://github.com/kolakao/bt4g-clicker/blob/master/bt4gautomagnetcopy.user.js
+// @updateURL    https://github.com/kolakao/bt4g-automatic-magnetlink-copy/blob/master/bt4gautomagnetcopy.meta.js
+// @downloadURL  https://github.com/kolakao/bt4g-automatic-magnetlink-copy/blob/master/bt4gautomagnetcopy.user.js
 // ==/UserScript==
 
 (function() {
